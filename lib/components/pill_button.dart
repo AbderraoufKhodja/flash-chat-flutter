9ce496e70c3a5ea0ecd7d311cb99a53f4073bb3f
KHODJA-ABDERRAOUF <201710800010@zjnu.edu.cn>
2020-07-28 20:49:27 +0800

    Code refactoring

---
+++ lib/components/pill_button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class PillButton extends StatelessWidget {
+  final String title;
+  final Function onPressed;
+  final Color fillColor;
+
+  PillButton(
+      {@required this.title,
+      @required this.fillColor,
+      @required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        elevation: 5.0,
+        color: fillColor,
+        borderRadius: BorderRadius.circular(30.0),
+        child: MaterialButton(
+          onPressed: onPressed,
+          minWidth: 200.0,
+          height: 42.0,
+          child: Text(
+            title,
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
